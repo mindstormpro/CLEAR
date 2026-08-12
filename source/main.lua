93ff5b471afdf2c9c0a1351a88cb7dc030b8b720
mindstormpro3 <elioanon20@proton.me>
2026-08-11 18:23:08 -0700

    added gitignore and cleared out code from main.lua

--- source/main.lua
+++ source/main.lua
@@ -6,10 +6,7 @@ import "CoreLibs/timer"
 local pd = playdate
 local gfx = pd.graphics
 
-local font = gfx.font.new("fonts/Nontendo-Bold-2x")
-playdate.graphics.setFont(font)
-
 function pd.update()
 
-    
+
 end
