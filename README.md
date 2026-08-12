e02d1b855b9b15861a00625da88764f5b63adab1
mindstormpro3 <elioanon20@proton.me>
2026-08-12 10:44:18 -0700

    Started making the 3D demo and added a README!

---
+++ README.md
@@ -0,0 +1,10 @@
+# CLEAR!
+clear is a tactical turn-based strategy game where you clear rooms of a level one-by-one by controlling the actions each squadmate makes.  
+Oh yeah, There's also wizards and fox people.
+
+Cool/notable features:
+ - Clearing of rooms
+ - Tactical stuff
+ - Replay mode (?) where you can watch your squad replay a level in real time instead of turn-by-turn (you have to actually beat the level first tho)
+ - ARCO inspired skill tree (?)
+ - Diora inspired 3D Graphix for Max Koolness!!!
--- source/main.lua
+++ source/main.lua
@@ -2,11 +2,34 @@ import "CoreLibs/object"
 import "CoreLibs/graphics"
 import "CoreLibs/sprites"
 import "CoreLibs/timer"
+import("blendate")
 
 local pd = playdate
-local gfx = pd.graphics
+local gfx = playdate.graphics
+
+--- TODOs:
+--- make tiles... well... tileable
+--- add dynamic loading
+--- make a bunch of tiles          IN PROGRESS
+--- stress test blendate and maybe rewrite the library to ditch the animation objects and just store the imagetable, x, y, and R values (and maybe z?)
+--- scale models down (by 2?)      IN PROGRESS
+
+local tiles = {}
+
+local tileMetadata = Blendate("tiles/TileMetadata.json")
+
+----------------TILES-----------------------
+----- FLOOR1
+--tiles.Floor1 = tileMetadata:loadRotation("tiles/Floor1")
+--tiles.Floor1.paused = true
+
+----- WALL1
+tiles.Wall1 = tileMetadata:loadRotation("tiles/Wall1")
+tiles.Wall1.paused = true
 
 function pd.update()
-
-
+    gfx.clear()
+    tiles.Wall1:draw(50, 30)
+    tiles.Wall1.frame = pd.getCrankPosition() / 2
+    print("frame #: " .. tiles.Wall1.frame)
 end
