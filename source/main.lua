32a73b81c0c166ced5b1e7a699695d14ab96ce9e
mindstormpro3 <elioanon20@proton.me>
2026-08-11 17:56:37 -0700

    first commit

---
+++ source/main.lua
@@ -0,0 +1,15 @@
+import "CoreLibs/object"
+import "CoreLibs/graphics"
+import "CoreLibs/sprites"
+import "CoreLibs/timer"
+
+local pd = playdate
+local gfx = pd.graphics
+
+local font = gfx.font.new("fonts/Nontendo-Bold-2x")
+playdate.graphics.setFont(font)
+
+function pd.update()
+
+
+end
